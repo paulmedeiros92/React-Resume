94dd1d8c7c3b07e63f0486eb544a3889a581eca7
PaulMed <paulmedeiros92@gmail.com>
2018-09-09 16:17:26 -0400

    Added routes and an input box that appears to use message reducers to carry messages although I have not confirmed yet.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// main routes
+import AppRoutes from './routes';
+import store from './store';
 
-// main app
-import App from './containers/App';
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <Provider store={store}>
+        <AppRoutes/>
+    </Provider>,
+    document.getElementById('app')
+);
